3a458a13e08f6381648a10e81ee530b2858636b0
rubennieto <40567804+rubennieto@users.noreply.github.com>
2018-06-25 14:27:02 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
-//* Añade imagen debajo de menú, encima del contenido en la front-page
+<?php   //* Añade imagen debajo de menú, encima del contenido en la front-page
 add_action( 'genesis_after_header', 'after_header_image' );
 
 function after_header_image() {
 
-if ( is_front_page() ) {
+    if ( is_front_page() ) {
 
-    $image = sprintf( '%s/images/after-header.png', get_stylesheet_directory_uri() );
-    
-    $output = sprintf( '<img src="%s" alt="Image" />', $image );
-    
-    echo $output;
-    }
+        $image = sprintf( '%s/images/after-header.png', get_stylesheet_directory_uri() );
+
+        $output = sprintf( '<img src="%s" alt="Image" />', $image );
+
+        echo $output;
+        }
 
 }
